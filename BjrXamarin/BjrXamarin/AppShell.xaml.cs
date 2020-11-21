cd2c6aaec1b13b34197587ca787500f50353b842
Monthas <monthewansi.herve@gmail.com>
2020-11-21 14:40:09 +0100

    Add project files.

---
+++ BjrXamarin/BjrXamarin/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using BjrXamarin.ViewModels;
+using BjrXamarin.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BjrXamarin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ BjrXamarin/BjrXamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BjrXamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BjrXamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
